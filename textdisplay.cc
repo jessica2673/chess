e3f02f27024069f22edafc458b6860ae70504932
Jiwon <94932131+jkim712@users.noreply.github.com>
2023-11-22 21:19:09 -0500

    Modified Piece

--- textdisplay.cc
+++ textdisplay.cc
@@ -23,25 +23,42 @@ TextDisplay::TextDisplay() {
             // if i is even, start with dark
             // if i is odd, start with white
             newRow.emplace_back(curSquare);
+
+            // toggle square
+            if (curSquare == darkSquare) {
+                curSquare = whiteSquare;
+            } else {
+                curSquare = darkSquare;
+            }
         }
         theDisplay.emplace_back(newRow);
     }
 }
 
 void TextDisplay::notify(Piece &p) {
-    
+    // insert here
 }
 
 TextDisplay::~TextDisplay() {
-
+    // insert here
 }
 
 ostream &operator<<(std::ostream &out, const TextDisplay &td) {
+<<<<<<< Updated upstream
     int size = td.boardSize;
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
             out << td.theDisplay[i][j];
         }
     }
+=======
+    for (int i = 0; i < td.boardSize; ++i) {
+        for (int j = 0; j < td.boardSize; ++j) {
+            out << td.theDisplay[i][j];
+        }
+        out << endl;
+    }
+
+>>>>>>> Stashed changes
     return out;
 }
