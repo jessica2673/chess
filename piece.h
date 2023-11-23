a8a8fee77bbd12b434adc84b9d2d9eee553d4f52
Jiwon <94932131+jkim712@users.noreply.github.com>
2023-11-22 21:44:10 -0500

    Update piece.h

--- piece.h
+++ piece.h
@@ -8,21 +8,17 @@
 enum class PieceType {Pawn, Bishop, Knight, Rook, Queen, King, Empty};
 
 class Piece { // Piece is a Subject
-  // bool isOn = false;
-  std::vector<Observer*> observers;
-  int row, col;
 
-  // Add other private members if necessary
-
- public:
+  public:
   Piece();  // Default constructor
+
+  enum class Colour { BLACK, WHITE };
 
   int getRow() const; // Returns the value of row.
   int getCol() const; // Returns the value of col.
   PieceType getState() const;
+  Colour getColour() const;
 
-  // void setOn();          // Explicitly sets me to on.
-  // void toggle();         // Toggles me between on and off.
   // void setCoords(int r, int c);  // Tells me my row and column number. MIGHT NEED THIS!?!?!????????????
 
   void attach(Observer *o);  // Attaches observers (TextDisplay and GraphicDisplay)
@@ -31,11 +27,17 @@ class Piece { // Piece is a Subject
 
   void notifyObservers();   // Piece will call this to let observers TextDisplay and GraphicDisplay know that they've moved so that the displays can update accordingly 
 
-
   virtual bool makeMove(int row, int col);
 
+  std::vector<Piece> checkAttackingMe();
+  virtual std::vector<int> checkPossibleMoves() = 0;
 
   virtual ~Piece() = 0; // makes the class abstract
 
+  private:
+    std::vector<Observer*> observers;
+    int row, col;
+    Colour colour;
+ 
 };
 #endif
