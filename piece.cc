833b0c3e384234f2bfe1730eda852dd5b29b0c31
Jiwon <94932131+jkim712@users.noreply.github.com>
2023-11-22 21:56:15 -0500

    Update piece.h

---
+++ piece.cc
@@ -0,0 +1,37 @@
+#include "piece.h"
+
+Piece::Piece(PieceType type, Colour colour);  // Default constructor
+
+// Returns the value of row.
+int Piece::getRow() const {
+    return row;
+}
+
+// Returns the value of col.
+int Piece::getCol() const {
+    return col;
+} 
+
+PieceType Piece::getState() const {
+    return type;
+}
+
+Colour Piece::getColour() const {
+    return colour;
+}
+
+// void setCoords(int r, int c);  // Tells me my row and column number. MIGHT NEED THIS!?!?!????????????
+
+void Piece::attach(Observer *o) {
+    observers.emplace_back(o);
+}  // Attaches observers (TextDisplay and GraphicDisplay)
+
+void Piece::detach(Observer *o) {
+
+}  // Attaches observers (TextDisplay and GraphicDisplay) DON"T REALLY NEED THIS LOLLLL
+
+// Piece will call this to let observers TextDisplay and GraphicDisplay know that they've moved so that the displays can update accordingly 
+void Piece::notifyObservers() {
+    for (auto p: observers) {
+	}
+}   
--- piece.h
+++ piece.h
@@ -6,38 +6,38 @@
 #include "observer.h"
 
 enum class PieceType {Pawn, Bishop, Knight, Rook, Queen, King, Empty};
+enum class Colour { BLACK, WHITE };
 
 class Piece { // Piece is a Subject
 
   public:
-  Piece();  // Default constructor
+    Piece(PieceType type, Colour colour);  // Default constructor
 
-  enum class Colour { BLACK, WHITE };
+    int getRow() const; // Returns the value of row.
+    int getCol() const; // Returns the value of col.
+    PieceType getState() const;
+    Colour getColour() const;
 
-  int getRow() const; // Returns the value of row.
-  int getCol() const; // Returns the value of col.
-  PieceType getState() const;
-  Colour getColour() const;
+    // void setCoords(int r, int c);  // Tells me my row and column number. MIGHT NEED THIS!?!?!????????????
 
-  // void setCoords(int r, int c);  // Tells me my row and column number. MIGHT NEED THIS!?!?!????????????
+    void attach(Observer *o);  // Attaches observers (TextDisplay and GraphicDisplay)
 
-  void attach(Observer *o);  // Attaches observers (TextDisplay and GraphicDisplay)
+    void detach(Observer *o);  // Attaches observers (TextDisplay and GraphicDisplay) DON"T REALLY NEED THIS LOLLLL
 
-  void detach(Observer *o);  // Attaches observers (TextDisplay and GraphicDisplay) DON"T REALLY NEED THIS LOLLLL
+    void notifyObservers();   // Piece will call this to let observers TextDisplay and GraphicDisplay know that they've moved so that the displays can update accordingly 
 
-  void notifyObservers();   // Piece will call this to let observers TextDisplay and GraphicDisplay know that they've moved so that the displays can update accordingly 
+    virtual bool makeMove(int row, int col);
 
-  virtual bool makeMove(int row, int col);
+    std::vector<Piece> checkAttackingMe();
+    virtual std::vector<int> checkPossibleMoves() = 0;
 
-  std::vector<Piece> checkAttackingMe();
-  virtual std::vector<int> checkPossibleMoves() = 0;
-
-  virtual ~Piece() = 0; // makes the class abstract
+    virtual ~Piece() = 0; // makes the class abstract
 
   private:
     std::vector<Observer*> observers;
     int row, col;
     Colour colour;
+    PieceType type;
  
 };
 #endif
