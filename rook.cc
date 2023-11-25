49286bd32cb6d35b615adc5c4df0ccd268b6dfc7
Jessica <jessica2003mail@gmail.com>
2023-11-24 21:30:00 -0500

    finish Rook checkMovementValid function

--- rook.cc
+++ rook.cc
@@ -7,7 +7,33 @@ bool Rook::checkMovementValid(const Board &b, int newRow, int newCol) {
     int run = newRow - this->getRow();
     int boardSize = b.getBoardSize();
 
-    
+    if (rise > 0 && run > 0) {
+        return false;
+    } else if (rise > 0) {
+        for (int r = 0; r < rise; ++r) {
+            if (b.getPiece(getRow() + r, getCol()) != nullptr) {
+                return false;
+            }
+        }
+    } else if (rise < 0) {
+        for (int r = 0; r > rise; --r) {
+            if (b.getPiece(getRow() + r, getCol()) != nullptr) {
+                return false;
+            }
+        }
+    } else if (run > 0) {
+        for (int c = 0; c < run; ++c) {
+            if (b.getPiece(getRow(), getCol() + c) != nullptr) {
+                return false;
+            }
+        }
+    } else {
+        for (int c = 0; c > run; --c) {
+            if (b.getPiece(getRow(), getCol() + c) != nullptr) {
+                return false;
+            }
+        }
+    }
 
     // Moving the piece
     setRow(newRow);
