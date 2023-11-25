154d301a8f30818ad3ccdbab769f454235279336
lillian-mo <molillian05@gmail.com>
2023-11-24 20:18:33 -0500

    made pawn class

---
+++ pawn.cc
@@ -0,0 +1,35 @@
+#include "pawn.h"
+
+Pawn::Pawn(int row, int col, Colour colour): Piece{row, col, PieceType::Pawn, colour}{}
+
+bool Pawn::checkMovementValid(const Board &b, int newRow, int newCol) {
+    int rise = newCol - getCol();
+    int run = newRow - getRow();
+    bool moveMade = false;
+
+    if (rise < 1) {
+        moveMade = false;
+    }
+
+    if (!firstMove) {
+        if (rise == 1 && run == 0 && b.getPiece(newRow, newCol) == nullptr) {
+            moveMade = true;
+        if (rise == 1 && (run == 1 || run == -1) && b.getPiece(newRow, newCol) != nullptr) {
+            moveMade = true;
+        }
+    } else {
+        if (rise == 2 && run == 0 && b.getPiece(newRow - 1, newCol) == nullptr) {
+            moveMade = true;
+        }
+    }
+
+    if (moveMade) {
+        setRow(newRow);
+        setCol(newCol);
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
---
+++ pawn.h
@@ -0,0 +1,23 @@
+#ifndef __PAWN_H__
+#define __PAWN_H__
+#include <iostream>
+#include <vector>
+#include "textdisplay.h"
+#include "observer.h"
+#include "piece.h"
+class Piece;
+using namespace std;
+
+class Pawn : public Piece {
+    PieceType type = PieceType::Pawn;
+    bool firstMove = true;
+
+public:
+    Pawn(int row, int col, Colour colour);
+
+    bool checkMovementValid(const Board &b, int newRow, int newCol);
+    
+};
+
+
+#endif
